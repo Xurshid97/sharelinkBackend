7bb8497297ba7a658195d893a321b9d8f266464d
Xurshid97 <xurshedrabo@gmail.com>
2024-05-07 16:48:39 +0200

    the user created with access token correctly

--- linkshare/models.py
+++ linkshare/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class SiteUser(models.Model):
+    access_token = models.CharField(max_length=36, default=uuid.uuid4, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+    @classmethod
+    def create_with_access_token(cls):
+        # Create SiteUser instance with a new UUID access token
+        access_token = str(uuid.uuid4())
+        site_user = cls(access_token=access_token)
+        # Save the instance to the database
+        site_user.save()
+        return site_user
+    
+    def __str__(self):
+        return self.access_token
