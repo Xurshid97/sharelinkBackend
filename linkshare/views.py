29999ac6fdc552e09c12c56ccd0dc7fb27af80cc
Xurshid97 <xurshedrabo@gmail.com>
2024-05-14 13:47:02 +0200

    some changes on categories

--- linkshare/views.py
+++ linkshare/views.py
@@ -5,78 +5,48 @@ from rest_framework import viewsets
 from .models import Category, Link, SiteUser
 from .serializers import CategorySerializer, LinkSerializer, SiteUserSerializer
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 class SiteUserViewSet(viewsets.ModelViewSet):
     queryset = SiteUser.objects.all()
     serializer_class = SiteUserSerializer
 
-    '''
-    def create(self, request, *args, **kwargs):
-        access_token = request.headers.get('AccessToken')
-        print('token', access_token)
-        if access_token:
-            # If access token is provided, check if the user already exists
-            existing_user = SiteUser.objects.filter(access_token=access_token).first()
-
-            if existing_user:
-                # User already exists, return appropriate response
-                serializer = self.get_serializer(existing_user)
-                return Response(serializer.data)
-            else:
-                # If user does not exist, create new user and assign access token
-                # serializer = self.get_serializer(data=request.data)
-                # serializer.is_valid(raise_exception=True)
-                # self.perform_create(serializer)
-                # user = serializer.instance
-                user = SiteUser.objects.create()
-                user.access_token = access_token
-                user.save()
-                return Response(serializer.data)
-        else:
-            # If no access token provided, create new user and generate access token
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            user = serializer.instance
-            # Generate access token and save
-            user.generate_access_token()
-            return Response({"access_token": user.access_token}, status=201)
-    '''
     def list(self, request, *args, **kwargs):
-        queryset = SiteUser.objects.all()
-        access_token = request.headers.get('AccessToken')
-        print('token', access_token)
-        if access_token:
-            # If access token is provided, check if the user already exists
-            existing_user = SiteUser.objects.filter(access_token=access_token).first()
+        # first get request never sent with access token.
+        user = SiteUser.objects.create()
+        user.save()
+        return Response({"access_token": user.access_token}, status=201)
+    
+    # this post request is for registering user with already access token.
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        access_token = request.headers.get('Authorization')
+        if data and access_token:
+            username = data['username']
+            password = data['password']
+            user = User.objects.filter(username=username).first()
+            site_user = SiteUser.objects.get(access_token=access_token)
 
-            if existing_user:
-                # User already exists, return appropriate response
-                serializer = self.get_serializer(existing_user)
-                return Response(serializer.data)
+            if not user and not site_user.user and username and password:
+                my_user = User.objects.create(username=username, password=password)
+                my_user.save()
+                site_user.user = my_user
+                site_user.save()
+                return Response({"access_token": site_user.access_token, "message": "User saved successfully."})
             else:
-                # If user does not exist, create new user and assign access token
-                # serializer = self.get_serializer(data=request.data)
-                # serializer.is_valid(raise_exception=True)
-                # self.perform_create(serializer)
-                # user = serializer.instance
-                user = SiteUser.objects.create()
-                user.access_token = access_token
-                user.save()
-                return Response(serializer.data)
+                return Response({"message": "the user is already present go to log in"})
         else:
-            # If no access token provided, create new user and generate access token
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            user = serializer.instance
-            # Generate access token and save
-            user.create_with_access_token()
-            return Response({"access_token": user.access_token}, status=201)
+            return Response(serializer.errors)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    def list(self, request, *args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        site_user = SiteUser.objects.get(access_token=access_token)
+        categories = Category.objects.filter(user=site_user)
+        serializer = self.serializer_class(categories, many=True)
+        return Response({"categories": serializer.data}, status=201)
 
 class LinkViewSet(viewsets.ModelViewSet):
     queryset = Link.objects.all()
